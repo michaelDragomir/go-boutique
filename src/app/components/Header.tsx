96c2e67066334803d061dd72e0e113c3bc3c673d
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-08-21 17:37:16 -0700

    logo updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,16 +5,16 @@ import SubHeaderTabs from './SubHeaderTabs';
 
 const Header = () => {
 	return (
-		<div className='sticky w-full bg-slate-300 z-30 shadow-sm top-0 '>
-			<div className='py-4 border-b-[1px]'>
-				<div className='max-w-[1920px] mx-auto xl:px-20 md:px-2 px-4'>
+		<div className='sticky w-full bg-white z-30 shadow-sm top-0 '>
+			<div className=''>
+				<div className={`max-w-[1920px] mx-auto xl:px-20 md:px-2 px-4`}>
 					<div className='flex flex-row items-center justify-between gap-3 md:gap-0 '>
 						<Image
 							src='/assets/images/goImage.png'
-							width={125}
-							height={125}
+							width={75}
+							height={75}
 							alt='go Image'
-							className='rounded-lg'
+							className='rounded-lg object-cover'
 						/>
 						<div className='flex items-center'>
 							<form
@@ -24,7 +24,7 @@ const Header = () => {
 							>
 								<input
 									required
-									className='w-60 h-10 placeholder:text-slate-400 block bg-slate-100 border border-slate-300 rounded-l-lg py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
+									className={`w-[800px] h-10 placeholder:text-slate-400 block bg-slate-100 border border-slate-300 rounded-l-lg py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm`}
 									placeholder='Explore Go Boutique...'
 									type='text'
 									// value={inputFieldValue}
@@ -32,9 +32,9 @@ const Header = () => {
 								/>
 								<button
 									type='submit'
-									className='h-10 rounded-r-lg border border-gray-700 bg-gray-700 px-3 py-2 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-gray-900 hover:bg-gray-900 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-300'
+									className='h-10 rounded-r-lg border border-gray-700 bg-gray-800 px-3 py-2 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-gray-900 hover:bg-gray-900 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed disabled:border-gray-300 disabled:bg-gray-300'
 								>
-									GO
+									<span className='text-[#57F8AB]'>GO</span>
 								</button>
 							</form>
 						</div>
@@ -47,7 +47,7 @@ const Header = () => {
 					</div>
 				</div>
 			</div>
-			<div className='bg-white'>
+			<div className='bg-slate-100'>
 				<SubHeaderTabs />
 			</div>
 		</div>

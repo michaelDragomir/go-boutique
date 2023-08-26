87808147260df6b2fb710a53992f8eb5ca3fa969
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-08-26 15:11:27 -0700

    styling and added Link

--- src/app/components/NavItemsDropDown/NavItemsDropDown.tsx
+++ src/app/components/NavItemsDropDown/NavItemsDropDown.tsx
@@ -1,12 +1,17 @@
-const NavItemsDropDown = ({ props }: { props: any }) => {
-	console.log('PROPS', props);
+import Link from 'next/link';
 
+const NavItemsDropDown = ({ props }: { props: any }) => {
 	type String = any;
 
+	//TODO:
+	// item name for url input
+
 	return (
-		<div className='rounded-md drop-shadow-md w-[159px] bg-white text-sm absolute right-[-30px] top-[17px]'>
+		<div className='p-3 rounded-md drop-shadow-md w-[159px] bg-white text-sm absolute right-[-30px] top-[17px]'>
 			{props.map((item: String, idx: number) => (
-				<div key={idx}>{item.name}</div>
+				<div className='pb-2 font-light' key={idx}>
+					<Link href={`/${item.name}`}>{item.name}</Link>
+				</div>
 			))}
 		</div>
 	);

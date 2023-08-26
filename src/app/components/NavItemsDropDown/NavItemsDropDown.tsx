494afcda826599e3cd1bb65b95333fd90bf81c7c
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-08-26 15:22:12 -0700

    styling

--- src/app/components/NavItemsDropDown/NavItemsDropDown.tsx
+++ src/app/components/NavItemsDropDown/NavItemsDropDown.tsx
@@ -7,10 +7,17 @@ const NavItemsDropDown = ({ props }: { props: any }) => {
 	// item name for url input
 
 	return (
-		<div className='p-3 rounded-md drop-shadow-md w-[159px] bg-white text-sm absolute right-[-30px] top-[17px]'>
+		<div className='px-3 pt-3 rounded-md drop-shadow-md w-[159px] bg-white text-sm absolute right-[-30px] top-[17px]'>
 			{props.map((item: String, idx: number) => (
-				<div className='pb-2 font-light' key={idx}>
-					<Link href={`/${item.name}`}>{item.name}</Link>
+				<div
+					className='pb-2 font-light text-slate-500 hover:text-slate-800'
+					key={idx}
+				>
+					<Link href={`/${item.name}`}>
+						<span className='hover:border-b hover:border-b-goPink'>
+							{item.name}
+						</span>
+					</Link>
 				</div>
 			))}
 		</div>

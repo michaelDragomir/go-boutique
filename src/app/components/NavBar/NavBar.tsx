0445f8aed0a09849d97480499f36ae89e567d3c9
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-08-31 14:17:49 -0700

    responsive fixes

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -104,9 +104,9 @@ const Header = () => {
 						</div>
 						<div
 							onClick={searchModalToggleHandler}
-							className='z-50 border border-slate-500 flex items-center gap-1 cursor-pointer text-slate-500 rounded-md tablet:hidden text-sm p-[5px]'
+							className='phone:w-screen phone:justify-center z-50 border flex border-slate-500 gap-1 cursor-pointer text-slate-500 rounded-md tablet:hidden text-sm p-[5px] justify-start'
 						>
-							<span className='min-w-[100px]'>Search Modal</span>
+							<span className=''>Search Modal</span>
 							<IoSearchSharp className='text-goGreen iconSize5px' />
 						</div>
 						<div
@@ -117,10 +117,10 @@ const Header = () => {
 									: ''
 							} `}
 						/>
-						<div className='flex items-center lg:gap-4 phone:gap-4 xl:gap-8'>
+						<div className='phone:hidden tablet:flex items-center lg:gap-4 phone:gap-4 xl:gap-8'>
 							<div className='group text-black z-50 hover:bg-slate-100'>
 								<div className='phone:p-[5px] xl:p-[7px] border border-slate-500 flex flex-row items-center gap-1 cursor-pointer text-slate-500 rounded-md'>
-									<span className='text-sm pr-2 phone:hidden sm:block'>
+									<span className='text-sm pr-2 phone:hidden md:block'>
 										Account
 									</span>
 									<FaRegUser className='text-slate-500 iconSize5px' />
@@ -142,7 +142,7 @@ const Header = () => {
 								)}
 
 								<h4 className='border-r border-goPink cursor-pointer text-sm text-slate-500 flex items-center'>
-									<span className='mr-1 phone:hidden sm:block'>Favorites</span>
+									<span className='mr-1 phone:hidden md:block'>Favorites</span>
 								</h4>
 								<span className='text-slate-500 flex items-center text-sm pl-1'>
 									3
@@ -156,13 +156,59 @@ const Header = () => {
 											Cart
 										</h4>
 									</div>
-									<span className='tablet:absolute tablet:top-[36px] tablet:right-[10px] md:absolute md:top-[55px] md:right-[10px] lg:static text-slate-100 h-6 w-6 rounded-full flex items-center justify-center text-sm bg-slate-500 drop-shadow-[0_2px_2px_rgba(0,0,0,0.3)] animate-pulse'>
+									<span className='tablet:absolute tablet:top-[16px] tablet:right-[10px] md:absolute md:top-[36px] md:right-[10px] lg:static text-slate-100 h-6 w-6 rounded-full flex items-center justify-center text-sm bg-slate-500 drop-shadow-[0_2px_2px_rgba(0,0,0,0.3)] animate-pulse'>
 										1
 									</span>
 									<h4 className='phone:hidden lg:block xl:block text-sm font-bold pl-1 text-slate-500'>
 										item(s)
 									</h4>
 								</div>
+							</div>
+						</div>
+					</div>
+					<div className='tablet:hidden flex items-center lg:gap-4 phone:gap-4 xl:gap-8 justify-center'>
+						<div className='group text-black z-50 hover:bg-slate-100'>
+							<div className='phone:p-[5px] xl:p-[7px] border border-slate-500 flex flex-row items-center gap-1 cursor-pointer text-slate-500 rounded-md'>
+								<span className='text-sm pr-2 sm:block'>Account</span>
+								<FaRegUser className='text-slate-500 iconSize5px' />
+								<IoIosArrowDown className='group-hover:hidden text-goGreen iconSize5px' />
+								<IoIosArrowUp className='hidden group-hover:block group-hover:text-goPink text-slate-500 iconSize5px' />
+							</div>
+							<div className='absolute invisible group-hover:visible group-hover:animate-slideDown'>
+								<NavAcctDropDown />
+							</div>
+						</div>
+						<div className='flex border border-slate-500 rounded-md  phone:p-[3px] xl:p-[7px] hover:border-slate-800'>
+							{FavoritesList.length > 0 ? (
+								<GoHeart className='text-goPink iconSize6px pr-1' />
+							) : (
+								<GoHeartFill
+									style={{ fill: '#FF66C4' }}
+									className='iconSize6px pr-1 animate-pulse'
+								/>
+							)}
+
+							<h4 className='border-r border-goPink cursor-pointer text-sm text-slate-500 flex items-center'>
+								<span className='mr-1 sm:block'>Favorites</span>
+							</h4>
+							<span className='text-slate-500 flex items-center text-sm pl-1'>
+								3
+							</span>
+						</div>
+						<div className='cursor-pointer text-black'>
+							<div className='flex items-center'>
+								<div className='md:relative lg:border-r lg:border-slate-400 mr-1'>
+									<IoCartOutline className='text-slate-500 iconSize8px' />
+									<h4 className='phone:hidden lg:block xl:block text-sm pr-1 text-slate-500 flex justify-center items-center'>
+										Cart
+									</h4>
+								</div>
+								<span className='phone:relative phone:top-[-10px] phone:right-[15px] tablet:absolute tablet:top-[36px] tablet:right-[10px] md:absolute md:top-[55px] md:right-[10px] lg:static text-slate-100 h-6 w-6 rounded-full flex items-center justify-center text-sm bg-slate-500 drop-shadow-[0_2px_2px_rgba(0,0,0,0.3)] animate-pulse'>
+									1
+								</span>
+								<h4 className='phone:hidden lg:block xl:block text-sm font-bold pl-1 text-slate-500'>
+									item(s)
+								</h4>
 							</div>
 						</div>
 					</div>

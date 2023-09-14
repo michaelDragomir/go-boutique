2c7ee11c1e6b7c7cdb27900ed839469fcd251512
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-09-14 15:16:20 -0700

    newsletter logic

--- src/app/components/NewsLetterSIgnUp/NewsLetterSignUp.tsx
+++ src/app/components/NewsLetterSIgnUp/NewsLetterSignUp.tsx
@@ -1,64 +1,61 @@
-import { createTransport } from 'nodemailer';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const NewsLetterSignUp = () => {
-	const transporter = createTransport({
-		service: 'gmail',
-		auth: {
-			user: 'michaeldragomir@gmail.com',
-			pass: 'Dinkledoink1!',
-		},
-	});
+	type String = string;
+	const router = useRouter();
+	const [emailInput, setEmailInput] = useState<String>('');
 
-	const mailOptionsFun = (e: any) => {
-		const mailOptions = {
-			from: 'pierrelabichel@pierrelabiche.com',
-			to: e.target.value,
-			subject: 'Thanks for subscribing!',
-			text: 'Welcome to our email list. We will keep you updated with the latest news and promotions.',
-		};
+	const submitHandler = async (event: any) => {
+		event.preventDefault();
+		try {
+			const response = await fetch('/api/emailRoute', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					email: emailInput,
+				}),
+			});
 
-		return mailOptions;
-	};
-
-	const test = (e: any) => {
-		transporter.sendMail(mailOptionsFun(e), (error, info) => {
-			if (error) {
-				console.log('Error:', error);
-			} else {
-				console.log('Email sent:', info.response);
+			if (response.ok) {
+				router.push('/');
 			}
-		});
+		} catch (error) {}
 	};
 
 	return (
 		<>
-			<div className='text-center'>
-				<div>
-					<h2 className='uppercase pb-2'>
-						subscibe to our newsletter and receive an extra 15% off your first
-						order.
-					</h2>
-					<p className='text-[12px] text-zinc-500'>
-						BE THE FIRST TO KNOW ABOUT OUR LATEST NEWS AND PROMOTIONS.
-					</p>
+			<form onSubmit={submitHandler}>
+				<div className='text-center'>
+					<div>
+						<h2 className='uppercase pb-2'>
+							subscibe to our newsletter and receive an extra 15% off your first
+							order.
+						</h2>
+						<p className='text-[12px] text-zinc-500'>
+							BE THE FIRST TO KNOW ABOUT OUR LATEST NEWS AND PROMOTIONS.
+						</p>
+					</div>
 				</div>
-			</div>
-			<div className='text-center'>
-				<input
-					type='email'
-					placeholder='Enter your email'
-					onChange={(e) => test(e.target.value)}
-					required
-					className='w-72 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-[#BBA14F] shadow-sm rounded-full text-sm'
-				/>
-				<button
-					type='submit'
-					onClick={test}
-					className='w-20 ml-3 px-3 py-2 text-white text-sm bg-black hover:bg-zinc-600 rounded-full duration-150'
-				>
-					Sign Up
-				</button>
-			</div>
+				<div className='text-center'>
+					<input
+						id='emailField'
+						type='email'
+						placeholder='Enter your email'
+						value={emailInput}
+						onChange={(e) => setEmailInput(e.target.value)}
+						required
+						className='w-72 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-[#BBA14F] shadow-sm rounded-full text-sm'
+					/>
+					<button className='w-20 ml-3 px-3 py-2 text-white text-sm bg-black hover:bg-zinc-600 rounded-full duration-150'>
+						Sign Up
+					</button>
+				</div>
+			</form>
 		</>
 	);
 };

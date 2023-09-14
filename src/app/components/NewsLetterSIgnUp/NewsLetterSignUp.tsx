f55a4c2181c0f92fd4e9a15fdd8e04ea68b5c874
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-09-14 10:05:09 -0700

    code work

--- src/app/components/NewsLetterSIgnUp/NewsLetterSignUp.tsx
+++ src/app/components/NewsLetterSIgnUp/NewsLetterSignUp.tsx
@@ -3,8 +3,6 @@
 import Link from 'next/link';
 import { useState } from 'react';
 
-import Test from './Test';
-
 const NewsLetterSignUp = () => {
 	const [emailValue, setEmailValue] = useState<any>('');
 
@@ -13,35 +11,35 @@ const NewsLetterSignUp = () => {
 
 	return (
 		<>
-			<form action=''>
-				<div className='text-center'>
-					<div>
-						<h2 className='uppercase pb-2'>
-							subscibe to our newsletter and receive an extra 15% off your first
-							order.
-						</h2>
-						<p className='text-[12px] text-zinc-500'>
-							BE THE FIRST TO KNOW ABOUT OUR LATEST NEWS AND PROMOTIONS.
-						</p>
-					</div>
+			<div className='text-center'>
+				<div>
+					<h2 className='uppercase pb-2'>
+						subscibe to our newsletter and receive an extra 15% off your first
+						order.
+					</h2>
+					<p className='text-[12px] text-zinc-500'>
+						BE THE FIRST TO KNOW ABOUT OUR LATEST NEWS AND PROMOTIONS.
+					</p>
 				</div>
-				<div className='text-center'>
-					<input
-						type='email'
-						placeholder='Enter your email'
-						value={emailValue}
-						onChange={(e) => setEmailValue(e.target.value)}
-						required
-						className='w-72 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-[#BBA14F] shadow-sm rounded-full text-sm'
-					/>
+			</div>
+			<div className='text-center'>
+				<input
+					type='email'
+					placeholder='Enter your email'
+					value={emailValue}
+					onChange={(e) => setEmailValue(e.target.value)}
+					required
+					className='w-72 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-[#BBA14F] shadow-sm rounded-full text-sm'
+				/>
+				<button type='submit'>
 					<Link
 						href={`mailto:${emailValue}?subject=welcome to the pierre labiche newsletter&body=${emailBody}`}
 						className='w-20 ml-3 px-3 py-2 text-white text-sm bg-black hover:bg-zinc-600 rounded-full duration-150'
 					>
 						Sign Up
 					</Link>
-				</div>
-			</form>
+				</button>
+			</div>
 		</>
 	);
 };

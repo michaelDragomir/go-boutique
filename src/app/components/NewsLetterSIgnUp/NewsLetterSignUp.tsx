a13adfe0752aa8744a523682e0b505c8e78fc45e
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2023-09-15 11:37:58 -0700

    code update

--- src/app/components/NewsLetterSIgnUp/NewsLetterSignUp.tsx
+++ src/app/components/NewsLetterSIgnUp/NewsLetterSignUp.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const NewsLetterSignUp = () => {
 	type String = string;
-	const router = useRouter();
 	const [emailInput, setEmailInput] = useState<String>('');
 
 	const submitHandler = async (e: any) => {
@@ -23,7 +21,8 @@ const NewsLetterSignUp = () => {
 			});
 
 			if (response.ok) {
-				router.push('/');
+				console.log('hitting statement');
+				setEmailInput('');
 			}
 		} catch (error) {
 			console.log('error');
@@ -32,36 +31,35 @@ const NewsLetterSignUp = () => {
 
 	return (
 		<>
-			<form onSubmit={submitHandler}>
-				<div className='text-center'>
-					<div>
-						<h2 className='uppercase pb-2'>
-							subscibe to our newsletter and receive an extra 15% off your first
-							order.
-						</h2>
-						<p className='text-[12px] text-zinc-500'>
-							THIS FEATURE WORKS. TRY IT OUT...USING NODEMAILER.
-						</p>
-					</div>
+			<div className='text-center'>
+				<div>
+					<h2 className='uppercase pb-2'>
+						subscibe to our newsletter and receive an extra 15% off your first
+						order.
+					</h2>
+					<p className='text-[12px] text-zinc-500'>
+						THIS FEATURE WORKS. TRY IT OUT...USING NODEMAILER.
+					</p>
 				</div>
-				<div className='text-center'>
-					<input
-						id='emailField'
-						type='email'
-						placeholder='Enter your email'
-						value={emailInput}
-						onChange={(e) => setEmailInput(e.target.value)}
-						required
-						className='w-72 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-[#BBA14F] shadow-sm rounded-full text-sm'
-					/>
-					<button
-						type='submit'
-						className='w-20 ml-3 px-3 py-2 text-white text-sm bg-black hover:bg-zinc-600 rounded-full duration-150'
-					>
-						Sign Up
-					</button>
-				</div>
-			</form>
+			</div>
+			<div className='text-center'>
+				<input
+					id='emailField'
+					type='email'
+					placeholder='Enter your email'
+					value={emailInput}
+					onChange={(e) => setEmailInput(e.target.value)}
+					required
+					className='w-72 mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-[#BBA14F] shadow-sm rounded-full text-sm'
+				/>
+				<button
+					type='submit'
+					onClick={submitHandler}
+					className='w-20 ml-3 px-3 py-2 text-white text-sm bg-black hover:bg-zinc-600 rounded-full duration-150'
+				>
+					Sign Up
+				</button>
+			</div>
 		</>
 	);
 };
